7f0976bfdf6b535e9a01f7bfcad1e920183906d0
naihet <naiyossskorn@gmail.com>
2026-03-24 18:43:10 +0700

    add PR info and comment

--- main.py
+++ main.py
@@ -2,18 +2,45 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+# =========================
+# Health check endpoint
+# =========================
 @app.get("/")
 def root():
+    # server check
     return {"message": "server is running"}
 
+
+# =========================
+# GitHub Webhook endpoint
+# =========================
 @app.post("/webhook")
 async def github_webhook(request: Request):
+    # get payload from GitHub (JSON)
     payload = await request.json()
 
-    print("WEBHOOK HIT!!!")
-    print("ACTION:", payload.get("action"))
+    print("\n======== WEBHOOK HIT ========")
 
-    print("FULL PAYLOAD:")
-    print(payload)
+    # show event type such
+    action = payload.get("action")
+    print("ACTION:", action)
+
+    # Prevent error if not PR event
+    if "pull_request" not in payload:
+        print("Not a pull request event")
+        return {"status": "ignored"}
+
+    # get PR
+    pr = payload["pull_request"]
+
+    # info
+    print("PR Title:", pr.get("title"))
+    print("PR URL:", pr.get("html_url"))
+    print("Repo:", payload["repository"]["full_name"])
+
+    # get diff URL
+    print("Diff URL:", pr.get("diff_url"))
+
+    print("======== END ========\n")
 
     return {"status": "ok"}
