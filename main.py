49a85b01a64fab47154791746e7e794b3709b376
naihet <naiyossskorn@gmail.com>
2026-03-24 16:07:33 +0700

    add root endpoint

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "server is running"}
+
 @app.post("/webhook")
 async def github_webhook(request: Request):
     payload = await request.json()
